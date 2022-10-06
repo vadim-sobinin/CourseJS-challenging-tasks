bc6a809c14148f2dc70f83175b0b22c41f146691
vadim-sobinin <vadimsobinin@yandex.ru>
2022-10-06 20:20:21 +0300

    remake lesson06

--- script.js
+++ script.js
@@ -1,50 +1,83 @@
-'use strict'; 
+'use strict';
+// Version 2
+let attemps;
 
-const isNumber = function(num) {
-  return !isNaN(parseInt(num)) && isFinite(num);
-};
+function generateNumber() {
+  return Math.round(Math.random() * 100);
+}
 
+function countAttemps() {
+  if (attemps <= 1) {
+    return endGame("Opps... Attempts are over. Would you like to try again?");
+  } else{
+    return attemps--;
+  }
+}
 
-const startGame = function () {
+function isNumber(num) {
+  return (!isNaN(parseInt(num)) && isFinite(num));
+}
 
-  const rightNumber = Math.round(Math.random() * 100);
-  console.log(`The correct number is: ${rightNumber}`);
-
-  let guessedNumber;
-  let wantToPlay = true;
-
-  const checkNumber = function() {
-
-    if (!wantToPlay) {
-      return "Stop game";
-    }
+function makeGuess (message = `${attemps} attemps is left! Guess a number from 1 to 100:`) {
   
-    if (guessedNumber === null){
-      guessedNumber = undefined;
-      wantToPlay = false;
-      return alert("The game is over");
-    } else if (!isNumber(guessedNumber) || guessedNumber < 1 || guessedNumber > 100) {
-      guessedNumber = prompt("Guess a number from 1 to 100:");
-      checkNumber();
-    }
-    // guessedNumber = +guessedNumber;
-    if (guessedNumber == 0) {
-      return "Game over";
-    } else if (rightNumber == guessedNumber) {
-      wantToPlay = false;
-      guessedNumber = undefined;
-      return alert("Congratulations! You guessed it!");
-    } else if (guessedNumber < rightNumber) {
-      guessedNumber = prompt(`Right number is more than ${guessedNumber}, try again:`);
-      checkNumber();
-    } else if (guessedNumber > rightNumber) {
-      guessedNumber = prompt(`Right number is less than ${guessedNumber}, try again:`);
-      checkNumber();
-    }
-  };
+  let guessedNumber = prompt(message);
 
-  checkNumber();
-};
+  if (guessedNumber === null){
+    stopGame();
+  } else {
+    if (isNumber(guessedNumber)){
+      
+      if (+guessedNumber > 0 && guessedNumber <= 100){
+        return +guessedNumber;
+      } else{
+        return makeGuess("Your number MUST be between 1 and 100! Try again:");
+      }
+       
+    } else {
+      return makeGuess("Please enter a NUMBER!");
+    }
+  }
+
+}
+
+function checkNumber(guessedNumber, rightNumber){
+  // console.log(`Right number is ${rightNumber}`);
+  // console.log(`Guessed number is ${guessedNumber}`);
+  
+  if (attemps > 0) {
+    if (guessedNumber < rightNumber){
+      countAttemps();
+      checkNumber(makeGuess(`${attemps} attemps is left! Right number is more than ${guessedNumber}, try again:`), rightNumber);
+    } else if (guessedNumber > rightNumber){
+      countAttemps();
+      checkNumber(makeGuess(`${attemps} attemps is left! Right number is less than ${guessedNumber}, try again:`), rightNumber);
+    } else if (rightNumber === guessedNumber){
+      endGame("Congratulations! You guessed it! \n Do you want to play more?");
+    }
+  }
+  
+}
+
+function stopGame(){
+  return alert("Thanks for the game! Hope to see you again! Goodbye!");
+}
+
+function endGame(message){
+  const shalWeContinue = confirm(message);
+  if (shalWeContinue) {
+    startGame();
+  } else {
+    stopGame();
+  }
+}
+
+
+function startGame() {
+  attemps = 10;
+  const rightNumber = generateNumber();
+  console.log(`Right number is ${rightNumber}`);
+  checkNumber(makeGuess(), rightNumber);
+}
+
 
 startGame();
-
