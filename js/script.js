3250fdc3d5b389a4f64d03738e9986f96af18f47
vadim-sobinin <vadimsobinin@yandex.ru>
2022-10-27 01:11:14 +0300

    fix lesson19

--- js/script.js
+++ js/script.js
@@ -10,18 +10,26 @@ let active = false;
 let count = 50;
 let idInterval;
 
+document.querySelector("#reset").addEventListener("click" , () => {
+  active = false;
+  cancelAnimationFrame(idInterval);
+  count = 50;
+  elem.style.top = count + "px";
+});
+
 btn.addEventListener("click", () => {
   active = !active;
+  
 
   active ? idInterval = requestAnimationFrame(animate) : cancelAnimationFrame(idInterval);
 
-})
+});
 
 const animate = () => {
   count++;
   idInterval = requestAnimationFrame(animate);
 
-  if (count < 500) {
+  if (count < 700) {
     elem.style.top = count + "px";
   } else {
     cancelAnimationFrame(idInterval);
