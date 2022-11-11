8bc108325ca3426ef8f409c186208d31197a0887
vadim-sobinin <vadimsobinin@yandex.ru>
2022-11-11 12:52:49 +0300

    lesson 26 -challenging-tasks complete

--- js/script.js
+++ js/script.js
@@ -3,32 +3,63 @@ const fromURL = '../db.json';
 const toURL = 'https://jsonplaceholder.typicode.com/posts';
 
 const getData = (URL) => {
-  fetch(URL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', URL);
+  xhr.send();
+
+  xhr.onload = () => {
+    if (xhr.status != 200) {
+      console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      console.log('Data received successfully!');
+      console.log(xhr.response);
+      sendData(xhr.response, toURL);
+    }
+  };
+
+  xhr.onprogress = (event) => {
+    if (xhr.status == 200) {
+      if (event.lengthComputable) {
+        console.log(`Got ${event.loaded} from ${event.total} bytes`);
+      } else {
+        console.log(`Got ${event.loaded} bytes`);
       }
-      return response.json().then((data) => sendData(data, toURL));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    }
+  };
+
+  xhr.onerror = () => {
+    console.log('Request failed');
+  };
 };
 
 const sendData = (data, toURL) => {
-  fetch(toURL, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: { 'Content-Type': 'application/json; charset=utf-8' },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.status + ' status');
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', toURL);
+  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  xhr.send(data);
+
+  xhr.onload = () => {
+    if (xhr.status != 201) {
+      console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      console.log('Data sent successfully!');
+      console.log(xhr.response);
+    }
+  };
+
+  xhr.onprogress = (event) => {
+    if (xhr.status == 200) {
+      if (event.lengthComputable) {
+        console.log(`Sent ${event.loaded} from ${event.total} bytes`);
+      } else {
+        console.log(`Sent ${event.loaded} bytes`);
       }
-      return response.json();
-    })
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+    }
+  };
+
+  xhr.onerror = () => {
+    console.log('Data sent failed');
+  };
 };
 
 getData(fromURL);
